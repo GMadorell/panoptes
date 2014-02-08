3ba305c5bf416bbce8db5513e7b1f52f3a30ed6e
Skabed <gmadorell@gmail.com>
2014-02-08 16:14:18 +0100

    Rebasing.

--- src/macros/macro_file_parser.py
+++ src/macros/macro_file_parser.py
@@ -1,1 +1,13 @@
-__author__ = 'skabed'
+
+
+class MacroFileParser(object):
+    def parse_file(self, path):
+        """
+        Parses a file, returning a MacroCollection instance collecting all the macros inside that file.
+        """
+        content = self.__read_file_content(path)
+
+    def __read_file_content(self, path):
+        with open(path, "r") as read_file:
+            content = read_file.read()
+        return content
